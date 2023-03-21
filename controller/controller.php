f9d65d9e8efb395529b3eac45685da4fad6d6d70
michgunde <117133781+michgunde@users.noreply.github.com>
2023-03-21 14:17:06 +0900

    calendar

--- controller/controller.php
+++ controller/controller.php
@@ -1,6 +1,27 @@
-<!-- CONTROLLER - connects database (model) and view -->
 <?php
+
+require_once('./model/calendarManager.php');
 
 function showIndex(){
     require("./view/indexView.php");
+}
+
+function addCalendar($data) {
+    for ($i = 0; $i < count($data); $i++) {
+        $date = strip_tags($data[$i]['date']);
+        $hour = strip_tags($data[$i]['hour']);
+
+        $calendarManager = new CalendarManager();
+        $result = $calendarManager->insertCalendar($date, $hour);
+        if (!$result) {
+            throw new Exception("Unable to add entries");
+        }
+        header("location: index.php?action=loadCalendar");
+    }
+}
+
+function showCalendar($user_id) {
+        $calendarManager = new CalendarManager();
+        $result = $calendarManager->loadCalendar($user_id);
+        require('./view/calendarView.php');
 }
--- index.php
+++ index.php
@@ -7,10 +7,22 @@ try{
     $action = $_REQUEST['action'] ?? null;
     
     switch ($action){
-        
+        case "updateCalendar":
+            $data = $_REQUEST['data'] ?? "";
+            if ($data) {
+                $data = json_decode($data,true);
+                addCalendar($data);
+            } else {
+                throw new Exception ("No calender inputs submitted");
+            }
+            break;
+        case "loadCalendar":
+            $user_id = $_SESSION['user_id'] ?? 1;
+            showCalendar($user_id);
+            break;
         default:
-        showIndex();
-        break;
+            showIndex();
+            break;
     }
 } catch (Exception $e){
     $errorMsg = $e->getMessage();
