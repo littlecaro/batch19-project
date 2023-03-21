9b53bd76a44e233618269653287a2dd8432c0ffe
Alice Hong <58031197+teabeau@users.noreply.github.com>
2023-03-21 13:29:14 +0900

    Update template.php

--- view/template.php
+++ view/template.php
@@ -9,10 +9,9 @@ $_SESSION["userId"] = 1; ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="./public/css/chatStyle.css"> -->
     <script src="https://kit.fontawesome.com/9d1def913c.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../public/css/styleMain.css" />
-<!--     <link rel="stylesheet" href="./public/css/messenger.css"> -->
+        <link rel="stylesheet" href="./public/css/styleMain.css" />
+    <link rel="stylesheet" href="./public/css/messenger.css">
     <script>
         const userId = <?= $_SESSION["userId"] ?>
     </script>
