59d8613c9e9823ec27a2d2fda8a7189663bd1702
teabeau <hippohong@gmail.com>
2023-04-03 12:12:34 +0900

    scrollbar for sidebar

--- view/companyDashboardTemplate.php
+++ view/companyDashboardTemplate.php
@@ -13,6 +13,5 @@
 <body>
     <?php require("./view/components/companyHeader.php"); ?>
     <?php require("./view/components/companySidebar.php"); ?>
-    <?php require("./view/components/footer.php"); ?>
     <?= $content; ?>
 </body>
