528468b6700e8e28fe590382fc83acfd81b2704b
michgunde <117133781+michgunde@users.noreply.github.com>
2023-04-17 15:30:26 +0900

    fix booked meetings view and job listings city query

--- view/components/bookedMeetingCard.php
+++ view/components/bookedMeetingCard.php
@@ -8,7 +8,7 @@
         <div class="meetingSubCard">
         <form action="index.php?action=talentProfile" method="POST">
         <input type="hidden" name="talentID" value="<?=$bookedMeetings[$i]->userID?>">
-                <button><p><?= htmlspecialchars($bookedMeetings[$i]->first_name) . " " . !empty(($bookedMeetings[$i]->last_name) ? htmlspecialchars($bookedMeetings[$i]->last_name) : null)?> </p></button>
+                <button><p><?= htmlspecialchars($bookedMeetings[$i]->first_name) . " " . (!empty($bookedMeetings[$i]->last_name) ? htmlspecialchars($bookedMeetings[$i]->last_name) : null)?> </p></button>
         </form>
                 <p>-</p>
         <form action="index.php?action=jobListings&ListingId=<?= $bookedMeetings[$i]->jobID?>" method="POST">
